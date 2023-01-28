73753989035e2b10dd61548c1c9ada614477b19b
Anton Mihailov <101288121+AntonMickhailov@users.noreply.github.com>
2023-01-28 23:34:43 +0300

    update fdfdg

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-module.exports = function reverse(n) {
-  return + Math.abs(n).toString().split("").reverse().join("");
-};
+module.exports = function check(str, bracketsConfig) {
+  if (str === '') return true;
+  let oldStrLength = 0;
+  while (oldStrLength - str.length)  {
+    oldStrLength = str.length;
+    for (let i = 0 ; i < bracketsConfig.length ; i++) {
+      str = str.replaceAll(bracketsConfig[i][0] + bracketsConfig[i][1] , '');
+    }
+    if (str.length === 0) return true;
+  }
+  return false;
+}
